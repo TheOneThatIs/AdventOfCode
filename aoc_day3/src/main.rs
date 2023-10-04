d3fdf1231d42330535a38a6abeceaf77dc1f7862
Jonathan Jackson <theonethatis720@gmail.com>
2023-10-04 14:47:49 -0700

    Day 3, part 2 completed

--- aoc_day3/src/main.rs
+++ aoc_day3/src/main.rs
@@ -10,7 +10,8 @@ fn main() {
     let input = fs::read_to_string("input.txt").unwrap();
     
     
-    println!("{} houses received at least one present.", part1(&input));
+    println!("Part1: {} houses received at least one present.", part1(&input));
+    println!("Part2: {} houses received at least one present.", part2(&input));
 }
 
 fn part1(input: &str) -> usize {
@@ -30,6 +31,41 @@ fn part1(input: &str) -> usize {
         if !houses_visited.contains(&current_pos) {
             houses_visited.push(current_pos);
         }
+    }
+    houses_visited.len()
+}
+
+fn part2(input: &str) -> usize {
+    let mut houses_visited: Vec<Pos> = Vec::new();
+    let mut santa_current_pos: Pos = Default::default();
+    let mut robo_current_pos: Pos = Default::default();
+    let mut is_robo_santas_turn: bool = false;
+    
+    for input in input.chars() {
+        if !is_robo_santas_turn {
+            match input {
+                '^' => santa_current_pos.y -= 1,
+                '>' => santa_current_pos.x += 1,
+                'v' => santa_current_pos.y += 1,
+                '<' => santa_current_pos.x -= 1,
+                _ => continue,
+            }
+            if !houses_visited.contains(&santa_current_pos) {
+                houses_visited.push(santa_current_pos);
+            }
+        } else {
+            match input {
+                '^' => robo_current_pos.y -= 1,
+                '>' => robo_current_pos.x += 1,
+                'v' => robo_current_pos.y += 1,
+                '<' => robo_current_pos.x -= 1,
+                _ => continue,
+            }
+            if !houses_visited.contains(&robo_current_pos) {
+                houses_visited.push(robo_current_pos);
+            }
+        }
+        is_robo_santas_turn = !is_robo_santas_turn;
     }
     houses_visited.len()
 }
